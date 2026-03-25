3ec132bbf0b7f7c2213627c3dde35265f4332b21
Brandon Budhan <bran7astic@ool-18bd48e2.dyn.optonline.net>
2026-03-25 14:41:34 -0400

    completed backend APIs

--- server/controllers/pkmnControllers.js
+++ server/controllers/pkmnControllers.js
@@ -3,8 +3,118 @@ import { pool } from "../config/database.js";
 // Add CRUD functions here
 
 const addPkmn = async(req, res) => {
-    const insertQuery = `
+    
+
+    try {
+        const {nickname, nature, ability, moves} = req.body
+        
+        const insertQuery = `
         INSERT INTO pkmn (nickname, nature, ability, moves)
-        VALUES ()
-    `
+        VALUES ($1, $2, $3, $4)
+        RETURNING *
+        `
+        
+        const results = await pool.query(insertQuery, [nickname, nature, ability, moves])
+        
+        res.status(201).json(results.rows[0])
+    } catch (err) {
+        res.status(409).json({ error: err.message })
+    }
+
+}
+
+const getAllPkmn = async (req, res) => {
+    try {
+        
+        const readQuery = `
+            SELECT * FROM pkmn ORDER BY id ASC;
+        `
+
+        const results = await pool.query(readQuery)
+        res.status(200).json(results.rows)
+
+    } catch (err) {
+        res.status(409).json({error: err.message})
+    }
+}
+
+const getPkmnById = async (req, res) => {
+    try {
+        const id = req.params.id
+        const readQuery = `
+            SELECT * FROM pkmn WHERE id = $1;
+        `
+
+        const results = await pool.query(readQuery, [id])
+        res.status(200).json(results.rows[0])
+        
+    } catch (err) {
+        res.status(409).json({error: err.message})
+    }
+}
+
+const updatePkmn = async (req, res) => {
+    try {
+        const id = req.params.id
+        const {nickname, nature, ability, moves} = req.body
+
+        const updateQuery = `
+            UPDATE pkmn
+            SET 
+                nickname = $1, 
+                nature = $2,
+                ability = $3,
+                moves = $4
+            WHERE id = $5
+        `
+
+        const results = await pool.query(updateQuery, [nickname, nature, ability, moves, id])
+        res.status(200).json(results.rows[0])
+
+    } catch (err) {
+        res.status(409).json({error: err.message})
+    }
+}
+
+const deletePkmnById = async(req, res) => {
+    try {
+
+        const id = req.params.id 
+        const deleteQuery = `
+            DELETE FROM pkmn
+            WHERE id=$1
+        `
+
+        const results = await pool.query(deleteQuery, [id])
+        res.status(200).json(results.rows[0])
+
+    } catch (err) {
+
+    }
+}
+ 
+const clearPkmn = async(req, res) => {
+
+    try {
+        const deleteQuery = `
+            TRUNCATE TABLE pkmn;
+        `
+
+        const results = await pool.query(deleteQuery)
+        res.status(200).json(results.rows[0])
+
+    } catch (err) {
+        res.status(409).json({error: err.message})
+    }
+
+}
+
+
+export default {
+    addPkmn,
+    getAllPkmn,
+    getPkmnById,
+    updatePkmn,
+    deletePkmnById,
+    clearPkmn
 }
--- server/routes/pkmnRoutes.js
+++ server/routes/pkmnRoutes.js
@@ -1,17 +1,21 @@
 import express from 'express'
+import GiftsController from '../controllers/pkmnControllers.js'
 // import controller for custom items
 
 
 const router = express.Router()
 
 // define routes to get, create, edit, and delete items
-router.get('/', (req, res) => {
-    res.status(200).send(`
-            <h1>
-                No pkmn yet!!
-            </h1>
-        `)
-})
+router.get('/', GiftsController.getAllPkmn)
 
+router.get('/:id', GiftsController.getPkmnById)
+
+router.post('/', GiftsController.addPkmn)
+
+router.patch('/:id', GiftsController.updatePkmn)
+
+router.delete('/:id', GiftsController.deletePkmnById)
+
+router.delete('/', GiftsController.clearPkmn)
 
 export default router
